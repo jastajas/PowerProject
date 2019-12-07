ba096911dd07bb16442a8c6fdfcd054e07f5edf5
jastajas <jastajas@gmail.com>
2019-12-07 22:02:46 +0100

    Updates

---
+++ precache-manifest.2d58cc4de7840971c5bef333b9150d21.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2381ce362868f35417e3f921bbbcc0d1",
+    "url": "/PowerProject/index.html"
+  },
+  {
+    "revision": "a8e5e4bdfa4687878cc9",
+    "url": "/PowerProject/static/css/main.1854930c.chunk.css"
+  },
+  {
+    "revision": "edf4a41dab85326e8925",
+    "url": "/PowerProject/static/js/2.8a7a4f97.chunk.js"
+  },
+  {
+    "revision": "a8e5e4bdfa4687878cc9",
+    "url": "/PowerProject/static/js/main.849f7f31.chunk.js"
+  },
+  {
+    "revision": "a7eca1f373c8a63ec50b",
+    "url": "/PowerProject/static/js/runtime-main.74822451.js"
+  },
+  {
+    "revision": "c317b9c22081b5eda03576a1c6e90f34",
+    "url": "/PowerProject/static/media/arrow.c317b9c2.svg"
+  },
+  {
+    "revision": "32ea9380706b5512644769458c10c555",
+    "url": "/PowerProject/static/media/done.32ea9380.svg"
+  },
+  {
+    "revision": "4687d6cb9b7420e71eed5804a6fe9ad9",
+    "url": "/PowerProject/static/media/duration.4687d6cb.svg"
+  },
+  {
+    "revision": "336993977fca0b1f2d0f6f6486d63ac6",
+    "url": "/PowerProject/static/media/inProgress.33699397.svg"
+  },
+  {
+    "revision": "9f46ca128bea128763c9c4bec1bfb3ae",
+    "url": "/PowerProject/static/media/main.9f46ca12.jpg"
+  },
+  {
+    "revision": "bc67068ef47bc653349370fc69bffff9",
+    "url": "/PowerProject/static/media/rocket.bc67068e.svg"
+  },
+  {
+    "revision": "fdc1b07cf828a30b3e0683f0bb7eaae5",
+    "url": "/PowerProject/static/media/toDo.fdc1b07c.svg"
+  },
+  {
+    "revision": "71cfd92a1591ab2583ed2761a6b91c83",
+    "url": "/PowerProject/static/media/upload.71cfd92a.svg"
+  },
+  {
+    "revision": "209f1cd7709c32348c9687a680b688a5",
+    "url": "/PowerProject/static/media/user.209f1cd7.svg"
+  }
+]);
